151e4f7eb29db4cd9fb1edb1df021c61ad11ffd3
Mario Rocha <eng.mec.mario.rocha@gmail.com>
2022-05-03 22:37:26 -0300

    mapping my entities with JPA

--- src/main/java/com/dio/santander/bankline/api/model/Movement.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movement.java
@@ -2,11 +2,27 @@ package com.dio.santander.bankline.api.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movement")
 public class Movement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private LocalDateTime datetime;
 	private String description;
 	private Double value;
+	
+	@Enumerated(EnumType.STRING)
 	private MovementType type;
 	
 	public Integer getId() {
